d7c4949adb1ea2fff8dec998218b70417a5d59cd
Ayush Shreshthi <62636885+AyushShreshthi@users.noreply.github.com>
2026-05-21 18:37:37 +0530

    controllers and models

---
+++ controllers/user.controller.js
@@ -0,0 +1,28 @@
+import { User } from "../models/users.model.js";
+
+const registerUser = async (req, res) => {
+    try {
+        const {username, email, password} = req.body;
+
+        if(!username || !email || !password){
+            return res.status(400).json({message: "All fields are important"})
+        }
+
+        const existing = await User.findOne({email: email.toLowerCase() });
+
+        if(existing){
+            return res.status(400).json({message: "user already exists!!"});
+        }
+
+
+        const user = await  User.Create({
+            username,
+            email : email.toLowerCase(),
+            password,
+            loggedIn: false
+        })
+    }
+    catch(error){
+        res.status(500).json({message: "Internal Server Error", error: error.message})  
+    }
+} 
---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            minLength: 1,
+            maxLength: 30
+
+        },
+        
+        password: {
+            type: String,
+            rquired: true,
+            minLength: 6,
+            maxLength: 50
+        },
+
+        email : {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            
+        },
+
+        
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model("User", userSchema)
